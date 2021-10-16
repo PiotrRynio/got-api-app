6a1bef445765c433bd0d83b50983b28dbf8e262c
PiotrWR <pwrynio@gmail.com>
2021-10-17 00:07:02 +0200

    Add useHouseDetails function

---
+++ src/apiHooks/HouseDetails/useHouseDetails.ts
@@ -0,0 +1,19 @@
+import { useQuery, UseQueryResult } from 'react-query';
+import { HouseDetailsItemType } from './HouseDetailsItemType';
+import { API_URL } from 'constant/apiUrl';
+import { validateResponse } from '../validateResponse';
+import { toHouseDetailsItemFromDto } from './toHouseDetailsItemFromDto';
+
+export const useHouseDetails = (
+  houseId: number,
+): UseQueryResult<{
+  houseDetailsItem: HouseDetailsItemType;
+}> =>
+  useQuery([`HouseDetailsItem-${houseId}`], async () => {
+    const response = await fetch(`${API_URL}/houses/${houseId}`);
+    await validateResponse(response);
+    const fetchedData = await response.json();
+
+    const houseDetailsItem = toHouseDetailsItemFromDto(fetchedData);
+    return { houseDetailsItem };
+  });
