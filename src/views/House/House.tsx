95031dfd2056ad7b21035a10df141e79d7b57dba
PiotrWR <pwrynio@gmail.com>
2021-10-17 02:59:24 +0200

    Add getting id from url path House Details component

--- src/views/House/House.tsx
+++ src/views/House/House.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import Typography from 'components/atoms/Typography/Typography';
 import { TypographyTag } from 'components/atoms/Typography/TypographyTags';
 import HouseDetails from 'components/organisms/HouseDetails/HouseDetails';
 import { Wrapper } from './House.styles';
 
 const House = () => {
-  //TODO: get params from URL
+  const { pathname } = useLocation();
+  const itemId = Number(pathname.substring(pathname.lastIndexOf('/') + 1));
 
   return (
     <Wrapper>
       <NavLink to="/">Home</NavLink>
       <Typography typographyTag={TypographyTag.HEADING_2}>House:</Typography>
-      <HouseDetails houseId={8} />
+      <HouseDetails houseId={itemId} />
     </Wrapper>
   );
 };
