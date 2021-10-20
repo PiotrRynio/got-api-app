e35ef565fa8c14d60917f76e8e813ce734f8e6e6
PiotrWR <pwrynio@gmail.com>
2021-10-20 15:38:01 +0200

    Fix - remove empty files and duplicated provider code

--- src/views/App/App.tsx
+++ src/views/App/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import AppProviders from 'providers/AppProviders';
 import { Wrapper } from './App.styles';
 import Navbar from 'components/organisms/Navbar/Navbar';
 import Home from 'views/Home/Home';
@@ -8,22 +7,20 @@ import House from '../House/House';
 
 function App() {
   return (
-    <AppProviders>
-      <Wrapper>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/houses/:houseId">
-            <House />
-          </Route>
-          <Route path="/">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </Wrapper>
-    </AppProviders>
+    <Wrapper>
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/houses/:houseId">
+          <House />
+        </Route>
+        <Route path="/">
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Wrapper>
   );
 }
 
